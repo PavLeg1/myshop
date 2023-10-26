893f281e758f0fe12d1f6725be173bda6a554a98
PavLeg1 <pavlego2000@yandex.ru>
2023-10-26 16:09:18 +0300

    add webhooks and import to csv

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,49 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
+import csv
+import datetime
+from django.http import HttpResponse
+
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+order_stripe_payment.short_description = 'Stripe payment'
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not \
+              field.many_to_many and not field.one_to_many]
+    
+    # Записать первую строку с информацией заголовка
+    writer.writerow([field.verbose_name for field in fields])
+    
+    # Записать строки данных
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
 
 
 @admin.register(Order)
@@ -14,3 +54,4 @@ class OrderAdmin(admin.ModelAdmin):
     
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline] 
+    actions = [export_to_csv]
--- payment/urls.py
+++ payment/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from . import webhooks
+
 
 app_name = 'payment'
 
@@ -9,4 +11,6 @@ urlpatterns = [
     path('completed/', views.payment_completed, name='completed'),
 
     path('canceled/', views.payment_canceled, name='canceled'),
+
+    path('webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
 ]
